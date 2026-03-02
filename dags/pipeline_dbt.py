94a626bef20189c8838336bfd22422ca08a472c9
TeetyK <teetysupergame7484@gmail.com>
2026-03-02 21:05:23 +0700

    add: test dbt

---
+++ dags/pipeline_dbt.py
@@ -0,0 +1,15 @@
+from cosmos import DbtDag , ProjectConfig , ProfileConfig
+
+from cosmos import DbtDag, ProjectConfig, ProfileConfig
+
+# dbt_dag = DbtDag(
+#     project_config=ProjectConfig("/path/to/dbt_project"),
+#     profile_config=ProfileConfig(
+#         profile_name="my_motherduck_project",
+#         target_name="dev",
+#         profiles_yml_filepath="/path/to/profiles.yml"
+#     ),
+#     schedule_interval='@daily',
+#     start_date=datetime(2026, 3, 1),
+#     dag_id="dbt_motherduck_dag",
+# )
